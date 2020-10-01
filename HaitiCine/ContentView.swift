2a1118b7f90087bce8264104cdb60ad30e28ff7a
Juli0 <julio.fils1@gmail.com>
2020-10-01 14:41:30 -0400

    Play with swifUI

--- HaitiCine/ContentView.swift
+++ HaitiCine/ContentView.swift
@@ -3,22 +3,52 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack{
-            Text("Movies list").font(.largeTitle)
-                .foregroundColor(Color.blue)
-            Spacer()
-            Image("store")
-                .resizable()
-                .aspectRatio(contentMode: .fit).cornerRadius(20)
-            .clipShape(Circle())
-                .padding(.all)
-            Spacer()
-            VStack{
+            HStack( ){
 
-            Text(" Movie name")
-            .font(.title)
-            Text("Movie description")
-                .font(.body)
+                VStack{
+                   Image("user")
+                       .resizable()
+                       .aspectRatio(contentMode: .fit)
+                        .frame(idealWidth: 40, idealHeight: 40)
+                        .clipShape(Circle())
+                    }.frame(width: 90, height:90)
+                    
+                       VStack(alignment: .leading){
+                           Text("Julio JEAN FILS")
+                           .frame(maxWidth: .infinity,
+                                  alignment: .leading)
+                            .font(.headline)
+                        
+                           Text("@juliojeanfils")
+                           .frame(maxWidth: .infinity, alignment: .leading)
+                           .font(.body)
+                       }
+                    }
+            
+            
+                    Image("store")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit).cornerRadius(0)
+                    .clipShape(Rectangle())
+                        .padding(.all)
+                    
+            
+            HStack{
+              HStack (){
+                  Image("user")
+                      .resizable()
+                      .aspectRatio(contentMode: .fit)
+                        .frame(width: 40, height:  40, alignment: .leading)
+                       .clipShape(Circle())
+                    
+                    Text("Movie name")
+                        .frame(maxWidth: .infinity , alignment: .leading)
+                        .font(.body)
+                
+               }
+                  
             }
+            Spacer()
         }
   
     }
