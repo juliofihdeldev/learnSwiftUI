999c925f22a6536e9ba912e2c7dcb2438b13a05b
Juli0 <julio.fils1@gmail.com>
2020-10-01 13:25:04 -0400

    Initial Commit

---
+++ HaitiCine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HaitiCine
+//
+//  Created by Julio Jean Fils on 1/10/20.
+//  Copyright © 2020 Julio Jean Fils. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
